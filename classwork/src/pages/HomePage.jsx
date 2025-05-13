a05c57c7c2a6410e52b9bdaaa7138edd0b324cf9
AmanCH3 <armanachaudhary53@gmail.com>
2025-05-13 12:59:55 +0545

    feat : Footer and header

--- classwork/src/pages/HomePage.jsx
+++ classwork/src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Headers from '../layouts/Headers'
 
 export default function HomePage() {
   return (
     <div>
+      
       HomePage
     </div>
   )
--- classwork/src/routers/AppRouter.jsx
+++ classwork/src/routers/AppRouter.jsx
@@ -3,14 +3,19 @@ import {BrowserRouter,Routes , Route} from 'react-router-dom';
 import HomePage from '../pages/HomePage'
 import LoginPage from '../pages/LoginPage'
 import Register from '../pages/Register'
+import MainLayout from '../layouts/MainLayout';
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
      <Routes>
-        <Route path = "/" element = {<HomePage/>}> </Route>
-        <Route path = "/login" element = {<LoginPage/>}></Route>
-        <Route path = "/register" element = {<Register/>}></Route>
+        <Route element = {<MainLayout/>}>
+        {/* main layout will show in every routes below */}
+          <Route path = "/" element = {<HomePage/>}> </Route>
+           <Route path = "/login" element = {<LoginPage/>}></Route>
+           <Route path = "/register" element = {<Register/>}></Route>
+        
+        </Route>
      </Routes>
     </BrowserRouter>
   )
