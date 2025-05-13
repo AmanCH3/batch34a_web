02baa88fe82ccc6cdbeda49f7ae012429b4222c8
AmanCH3 <armanachaudhary53@gmail.com>
2025-05-13 12:34:44 +0545

    first commit

---
+++ classwork/src/routers/AppRouter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {BrowserRouter,Routes , Route} from 'react-router-dom';
+import HomePage from '../pages/HomePage'
+import LoginPage from '../pages/LoginPage'
+import Register from '../pages/Register'
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+     <Routes>
+        <Route path = "/" element = {<HomePage/>}> </Route>
+        <Route path = "/login" element = {<LoginPage/>}></Route>
+        <Route path = "/register" element = {<Register/>}></Route>
+     </Routes>
+    </BrowserRouter>
+  )
+}
